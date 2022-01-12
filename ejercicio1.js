dea6ef18a7c05a73a8dfc0517a4b72f24200709d
3G0one <3G0one@gmail.com>
2022-01-11 23:04:54 -0600

    first commit

---
+++ ejercicio1.js
@@ -0,0 +1,6 @@
+//ejercicio 1
+let edad = parseInt(prompt("Ingresa tu fecha de nacimiento"));
+let fecha = parseInt(prompt("Ingresa fecha a calcular"));
+let calc = fecha - edad;
+let calc2 = calc - 1;
+alert("Tu edad es: " + calc2 + " ó "+ calc + " años");
---
+++ team-exercise.js
@@ -0,0 +1,198 @@
+
+var x = false ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));
+
+// true
+
+var x = true ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));
+
+// 0
+var x = 0 ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));			
+
+// "0"
+var x = "0" ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));
+
+// "000"
+var x = "000" ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));
+
+// "1"
+var x = "1" ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));
+
+// NaN
+var x = NaN ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));
+
+// Infinity	
+var x = Infinity ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));	
+
+//-Infinity	
+var x = -Infinity ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));		
+
+//“”	
+
+var x = "" ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));
+
+//“20”	
+var x = "20" ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));	
+
+//“Twenty”	
+var x = "Twenty" ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));		
+
+//null	
+var x = null ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));	
+
+//undefinited
+var x = undefined ;
+
+var y = x.toString();
+var z =  Number(x);
+var w = Boolean(x);
+
+console.log(y);// print false as string
+console.log(typeof(y));// print false as string
+console.log(z);
+console.log(typeof(z));
+console.log(w);
+console.log(typeof(w));
+
